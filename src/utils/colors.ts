deb1f00665fbef0998ca809968428984b718ca03
absrdld <toniemitu@gmail.com>
2026-02-27 22:33:48 +0100

    update kolorów w utils

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,23 +1,23 @@
 export const getStyleColor = (style: string): string => {
   const map: Record<string, string> = {
-    'Renesans': '#F4CCCC',
-    'Manieryzm': '#FCE5CD',
-    'Barok': '#FFF2CC',
-    'Rokoko': '#D9EAD3',
-    'Klasycyzm': '#D0E0E3',
-    'Neoklasycyzm': '#D0E0E3', // Map Neoklasycyzm to Klasycyzm
-    'Romantyzm': '#CFE2F3',
+    'Renesans': '#FFC942',
+    'Manieryzm': '#FFC2EC',
+    'Barok': '#757575',
+    'Rokoko': '#CDF4D4',
+    'Klasycyzm': '#AD660A',
+    'Neoklasycyzm': '#AD660A',
+    'Romantyzm': '#F849C1',
     'Akademizm': '#D9D2E9',
-    'Realizm': '#EAD1DC',
-    'Impresjonizm': '#E6B8AF',
-    'Postimpresjonizm': '#F9CB9C',
+    'Realizm': '#1E7129',
+    'Impresjonizm': '#C2E5FF',
+    'Postimpresjonizm': '#874FFF',
     'Symbolizm': '#B6D7A8',
     'Secesja': '#A2C4C9',
     'Fowizm': '#F6B26B',
     'Ekspresjonizm': '#FFE599',
     'Kubizm': '#B4A7D6',
     'Surrealizm': '#D5A6BD',
-    'Sztuka Japońska': '#E0E0E0',
+    'Sztuka Japońska': '#FFC2EC',
   };
   return map[style] || '#f5f5f5';
 };
